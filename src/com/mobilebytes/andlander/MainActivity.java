abd259284656124cf276e8efc8ad7f8fe720e179
Fred Grott <fred.grott@gmail.com>
2010-10-06 01:41:55 -0500

    more changes to support dashboard UI pattern

--- src/com/mobilebytes/andlander/MainActivity.java
+++ src/com/mobilebytes/andlander/MainActivity.java
@@ -1,13 +1,39 @@
 package com.mobilebytes.andlander;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.dashboard);
+
+
+
+
+        findViewById(R.id.Go).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.Info).setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+
+
 }
